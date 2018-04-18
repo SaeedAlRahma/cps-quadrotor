79150e420e3c47cbee5681c40752d347dba9bcaf
Tripp Whaley <twhaley215@gmail.com>
2018-04-18 07:03:41 -0400

    Update path_planner.py
    Moved get_dist() and calculations of edge weights to the Graph class where they should be, also removed unnecessary data structures to

--- path_planner/path_planner.py
+++ path_planner/path_planner.py
@@ -12,44 +12,40 @@ class Plane():
         self.center = self.get_centroid(vertices)
         self.vertices = vertices
         self.edges = list(itertools.combinations(vertices, 2))
-        self.neighbors = []
 
 
     def get_centroid(self, vertices):
         return ((vertices[0][0] + vertices[1][0] + vertices[2][0])/3, (vertices[0][1] + vertices[1][1] + vertices[2][1])/3) #can extend to z coords if necessary
 
 
-
 class Graph():
     def __init__(self, planes):
         self.planes = planes
-        ## Realistically all this should be its own data structure, but that's for later...
         self.edge_center_map = {}
-        self.edge_list = []
         self.edge_weights = {}
-        self.edge_label_map = {}
         for plane in self.planes:
             self.get_adjacent(plane)
+        for edge in self.edge_center_map.keys():
+            self.edge_weights[edge] = self.get_dist(self.edge_center_map[edge][0], self.edge_center_map[edge][1])
 
 
-    # This is hot garbage but I'm tired and it works
+    def get_dist(self, source, target):
+        return np.sqrt((source[0] - target[0])**2 + (source[1] - target[1])**2)
+
+
     def get_adjacent(self, plane):
-        neighbors = []
         for temp in self.planes:
             if (temp != plane):
                 for e1, e2 in itertools.product(temp.edges, plane.edges):
                     if ((e1[0] == e2[0] and e1[1] == e2[1]) or (e1[0] == e2[1] and e1[1] == e2[0])):
-                        if (e1 not in self.edge_list):
-                            self.edge_list.append(e1)
+                        if (e1 not in self.edge_center_map.keys()):
                             self.edge_center_map[e1] = (temp.center, plane.center)
-                            self.edge_label_map[e1] = (temp.label, plane.label)
-                        neighbors.append((temp.center, plane.center))
-        plane.neighbors = neighbors
         return
 
 
     def get_plane_from_label(self, label):
         return next(obj for obj in self.planes if obj.label==label)
+
 
     def get_plane_from_center(self, center):
         return next(obj for obj in self.planes if obj.center==center)
@@ -58,12 +54,6 @@ class Graph():
 class Path_Planner():
     def __init__(self, graph):
         self.graph = graph
-        for edge in self.graph.edge_center_map.keys():
-            self.graph.edge_weights[edge] = self.get_dist(self.graph.edge_center_map[edge][0], self.graph.edge_center_map[edge][1])
-
-
-    def get_dist(self, source, target):
-        return np.sqrt((source[0] - target[0])**2 + (source[1] - target[1])**2)
 
 
     def dijkstra(self, start_label, goal_label):
@@ -72,7 +62,7 @@ class Path_Planner():
         goal = self.graph.get_plane_from_label(goal_label).center
         g = defaultdict(list)
         edges = []
-        for edge in self.graph.edge_list:
+        for edge in self.graph.edge_center_map.keys():
             planes = self.graph.edge_center_map[edge]
             edges.append((planes[0], planes[1], self.graph.edge_weights[edge]))
         for l,r,c in edges:
