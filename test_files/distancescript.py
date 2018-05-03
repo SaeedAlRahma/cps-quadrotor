4af1754f04cbce864ee5125ed6a00e4228364c27
agd19 <agd19@duke.edu>
2018-05-03 17:29:35 -0400

    added method to make max times

--- test_files/distancescript.py
+++ test_files/distancescript.py
@@ -19,6 +19,18 @@ AB_INDEX = 0
 BC_INDEX = 1
 AC_INDEX = 2
 
+NOISE1  = 0.218755358604
+NOISE2  = 0.416135222849
+NOISE3  = 0.575114018097
+NOISE4  = 0.0565917003624
+NOISE5  = 0.124517371433
+NOISE6  = 0.910000809258
+NOISE7  = 1.67250197011
+NOISE8  = 0.515976111076
+NOISE10 =  0.31362093354
+
+STABLIZE_TIME = 2.86
+
 # Creates line vals given two points
 def getLine(x_i, y_i, x_f, y_f):
     points = [(x_i,y_i),(x_f,y_f)]
@@ -99,6 +111,26 @@ def printTimes(times, filename):
         dataFile.write("\n")
     dataFile.close()
 
+def addnoise(dists):
+    noisyDists = []
+    for item in dists
+        if item <= 1.5 then noisyDists.append(item + NOISE1)
+        elif item <= 2.5 then noisyDists.append(item + NOISE2)
+        elif item <= 3.5 then noisyDists.append(item + NOISE3)
+        elif item <= 4.5 then noisyDists.append(item + NOISE4)
+        elif item <= 5.5 then noisyDists.append(item + NOISE5)
+        elif item <= 6.5 then noisyDists.append(item + NOISE6)
+        elif item <= 7.5 then noisyDists.append(item + NOISE7)
+        elif item <= 8.5 then noisyDists.append(item + NOISE8)
+        else noisyDists.append(item + NOISE10)
+
+def maxTimes(centroidArr):
+    maxTimeArr = []
+    for item in centroidArr
+        dists = addnoise(item[2:4])
+        times = (dists/SPEED) + STABLIZE_TIME
+        maxTimeArr.append(item[0], max[times])
+    return maxTimeArr
 
 def calcEnterTime(centroidArr):
     #[zone] [dist leaving ab    dist leaving bc     dist leaving ac]
