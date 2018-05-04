3b1a311768cd63c2738243ea8f7df0f7e4dc066e
Tripp Whaley <twhaley215@gmail.com>
2018-05-03 23:17:52 -0400

    small change to the drawer in path_planner

--- path_planner/path_drawer.py
+++ path_planner/path_drawer.py
@@ -1,7 +1,7 @@
 from graphics import *
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-win = GraphWin("path_planner", SCREEN_WIDTH, SCREEN_HEIGHT)
+win = GraphWin("path_planner", SCREEN_WIDTH+100, SCREEN_HEIGHT+100)
 
 ## This needs work with screen size and scaling to be actually usable, but the basic idea works
 class Drawer():
@@ -20,15 +20,15 @@ class Drawer():
             edges.append(p.edges)
         for v in vertices:
             for v2 in v:
-                Circle(Point(v2[0]*SCREEN_WIDTH/max_x, SCREEN_HEIGHT - v2[1]*SCREEN_HEIGHT/max_y), 5).draw(win)
+                Circle(Point(v2[0]*SCREEN_WIDTH/max_x + 50, SCREEN_HEIGHT - v2[1]*SCREEN_HEIGHT/max_y + 50), 5).draw(win)
         for e in edges:
             for e2 in e:
-                Line(Point(e2[0][0]*SCREEN_WIDTH/max_x, SCREEN_HEIGHT - e2[0][1]*SCREEN_HEIGHT/max_y), Point(e2[1][0]*SCREEN_WIDTH/max_x, SCREEN_HEIGHT - e2[1][1]*SCREEN_HEIGHT/max_y)).draw(win)
+                Line(Point(e2[0][0]*SCREEN_WIDTH/max_x + 50, SCREEN_HEIGHT - e2[0][1]*SCREEN_HEIGHT/max_y + 50), Point(e2[1][0]*SCREEN_WIDTH/max_x + 50, SCREEN_HEIGHT - e2[1][1]*SCREEN_HEIGHT/max_y + 50)).draw(win)
         p3 = None
         p4 = None
         for p1, p2 in path:
             if ((p3 is not None and p4 is not None) and (p1,p2) is not path[0] and (p3,p4) is not path[-1]):
-                Line(Point(p1*SCREEN_WIDTH/max_x, SCREEN_HEIGHT - p2*SCREEN_HEIGHT/max_y), Point(p3*SCREEN_WIDTH/max_x, SCREEN_HEIGHT - p4*SCREEN_HEIGHT/max_y)).draw(win)
+                Line(Point(p1*SCREEN_WIDTH/max_x + 50, SCREEN_HEIGHT - p2*SCREEN_HEIGHT/max_y + 50), Point(p3*SCREEN_WIDTH/max_x + 50, SCREEN_HEIGHT - p4*SCREEN_HEIGHT/max_y + 50)).draw(win)
             p3, p4 = p1, p2
         try:
             win.getMouse()
